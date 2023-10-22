ef4689e160257dc7350ecd37d6ceda579b8b1e18
decimozs <marlonadiguemartin548@gmail.com>
2023-10-22 11:41:25 +0800

    commit : get yo ass up

---
+++ get yo ass up on java/KidsWithTheGreaterNumberOfCandies.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.lang.Math;
+
+/**
+ * KidsWithTheGreaterNumberOfCandies
+ */
+
+ public class KidsWithTheGreaterNumberOfCandies {
+    public static List<Boolean> kidsWithCandies (int[] candies, int extraCandies) {
+        int max = 0;
+
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+
+        List<Boolean> arrayOfCandies = new ArrayList<>();
+
+        for (int candy : candies) {
+            arrayOfCandies.add(candy + extraCandies >= max);
+        }
+
+        return arrayOfCandies;        
+    }
+
+    public static void main(String[] args) {
+        int[] candies = {2,3,5,1,3};
+        List<Boolean> result = kidsWithCandies(candies, 3);
+        System.out.println(result);
+    }
+}
